fca71721ce6b01ab678094371418e42faed8ad70
CupIvan <mail@cupivan.ru>
2011-12-18 01:26:39 +0400

    отображение недостающих продуктов в списке симпика новый дизайн списка

--- functions/simpList.js
+++ functions/simpList.js
@@ -28,27 +28,37 @@ if (page('verkauf.php')) (function(){
 var round = function(x, d)
 {
 	d = Math.pow(10, d);
-	return Math.round(x * d) / d;
+	x = Math.round(x * d);
+	return x / d + (x%100==0?'.0':'') + (x%10==0?'0':'');
 }
 var id   = kundenId;
 var list = $$('.blau');
 var div  = list[list.length - 1];
 var sum  = parseInt(div.innerHTML.replace(/\D/g, '')) / 100;
 
-var productId, price, st = '', sum_real = 0, s;
+var productId, price, st = '', sum_real = 0, s, num, cl_color, st_right;
 var list  = storage.get('/session/simp/'+id);
 
+st_right = ' style="text-align: right"';
 for (productId in list)
 {
 	price = storage.get('/market/price/' + productId, 0);
-	sum_real += s = round(list[productId] * price, 2);
-	st += '<div class="e'+productId+'" style="display: inline-block;"></div> '+
-		'<span class="blau">'+list[productId] + 'x' + price + ' = ' + s + '</span><br>';
+	sum_real += s = list[productId] * price;
+	num = parent.regal.getCount(productId);
+	if (num < list[productId]) num -= list[productId]; // если не хватает - пишем отрицательное число
+	cl_color =  'class="' + (num>0?'blau':'rot') + '"';
+	st += '<tr>'+
+		'<td><div class="e' + productId + '" style="display: inline-block;"></div></td>'+
+		'<td '+cl_color+st_right+'>' + num   + ' x</td>'+
+		'<td '+cl_color+'>' + price + '</td>'+
+		'<td '+cl_color+'>' + '=' + '</td>'+
+		'<td '+cl_color+st_right+'>' + round(s, 2)    + '</td>'+
+		'</tr>';
 }
 
 var div = $$('div');
-style(div[1], 'text-align: left; padding: 0 0 0 30px; margin-top: -30px');
-div[1].innerHTML = st;
+style(div[1], 'margin-top: -30px');
+div[1].innerHTML = '<table style="margin: auto">'+st+'</table>';
 $(div[div.length - 2], sum + ' / '+ round(sum_real, 2));
 
 })();
